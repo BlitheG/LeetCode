3014969b34b9448918b607714676bc85397715e1
Gubn <1207201510@qq.com>
2021-07-08 16:01:28 +0800

    add leetcode 0005 最长回文子串

---
+++ 0005 最长回文子串/Golang/0005 最长回文子串.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func longestPalindrome(s string) string {
+    if (s == "" || len(s) == 0) {
+        return ""
+    }
+
+    // 保存起始位
+    var str_range [2] int
+    var str = []string(s)
+    for i:=0; i < len(s); i++ {
+        // 把回文看成中间的部分全是同一字符，左右部分相对称
+        // 找到下一个与当前字符不同的字符
+        i = findLongest(str, i, str_range)
+    }
+	fmt.Println(str_range[0], str_range[1] + 1)
+    return s[str_range[0]: str_range[1] + 1]
+}
+
+
+
+func findLongest(str []string, low int, str_range [2]int) int {
+	// 查找中间部分
+	var high = low
+	for (high < len(str) - 1 && str[high + 1] == str[low]) {
+		high++
+	}
+
+	// 定位中间部分的最后一个字符
+	var ans = high
+
+	// 从中间向左右扩散
+	for (low > 0 && high < len(str) - 1 && str[low - 1] == str[high + 1]) {
+		low--
+		high++
+	}
+
+	// 记录最大长度
+	if (high - low > str_range[1] - str_range[0]) {
+		str_range[0] = low
+		str_range[1] = high
+	}
+	return ans
+}
+
+func main() {
+	var s = "babad"
+	longestPalindrome(s)
+}
